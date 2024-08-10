dbd2f260692073980d2f0a74707b1b3b2dbf5bd7
denizbee <129987653+denizbee@users.noreply.github.com>
2024-08-10 21:11:49 +0300

    Added comments plugin

--- init.lua
+++ init.lua
@@ -173,6 +173,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  { 'numToStr/Comment.nvim', opts = {} },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
