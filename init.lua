3a9f83a7be916a81faf6b7dd9a37ccc99bb554e1
denizbee <129987653+denizbee@users.noreply.github.com>
2024-10-08 18:22:00 +0300

    Added clangd

--- init.lua
+++ init.lua
@@ -634,10 +634,11 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
         javascript = { 'prettierd' },
         html = { 'prettierd' },
         vue = { 'prettierd' },
-        -- Conform can also run multiple formatters sequentially
         python = { 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
