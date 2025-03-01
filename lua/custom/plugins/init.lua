d5ad2b0ec92566d4e2a7e062c6e7136da2d1ba7a
denizbee <bayramogludeniz1997@gmail.com>
2025-03-02 01:52:30 +0300

    Added lualine

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -33,4 +33,16 @@ return {
       }
     end,
   },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        section_separators = '',
+        component_separators = '',
+        globalstatus = true,
+      },
+    },
+  },
 }
